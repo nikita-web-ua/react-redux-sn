353e266a29c51070cdbac0eab50ba5a5f61d7ef4
Nikita <maliarchuk99@gmail.com>
2021-08-25 17:25:15 +0300

    Constructing Login form using redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,40 @@
+import {Field, reduxForm} from "redux-form";
 
-let Login = (props) => {
-    return <div><h1>Login</h1></div>
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={"input"}/></div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={"input"}/></div>
+            <div>
+                <Field type={"checkbox"} name={"rememberMe"} component={"input"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({
+    // a unique name for the form
+    form: 'login'
+})(LoginForm)
+
+
+const Login = (props) => {
+
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+    )
 }
 
 export default Login
