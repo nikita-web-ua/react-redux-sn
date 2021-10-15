3cefbb1fb6bec3af9e5ea9f2733a6920aeeecfba
Nikita Maliarchuk <maliarchuk@Nikitas-MacBook-Air.local>
2021-10-15 14:30:20 +0200

    React.lazy Suspense for DialogsContainer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter, Route} from "react-router-dom";
-import Settings from "./components/Settings/Settings";
-import Music from "./components/Music/Music";
-import News from "./components/News/News";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import Login from "./components/Login/Login";
-import React, {Component} from "react";
-import {connect, Provider} from "react-redux";
-import {initializeApp} from "./redux/app-reducer";
-import Preloader from "./components/common/Preloader/Preloader";
-import store from "./redux/redux-store";
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import {BrowserRouter, Route} from "react-router-dom"
+import Settings from "./components/Settings/Settings"
+import Music from "./components/Music/Music"
+import News from "./components/News/News"
+import UsersContainer from "./components/Users/UsersContainer"
+import ProfileContainer from "./components/Profile/ProfileContainer"
+import HeaderContainer from "./components/Header/HeaderContainer"
+import Login from "./components/Login/Login"
+import React, {Component, Suspense} from "react"
+import {connect, Provider} from "react-redux"
+import {initializeApp} from "./redux/app-reducer"
+import Preloader from "./components/common/Preloader/Preloader"
+import store from "./redux/redux-store"
+
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"))
 
 class App extends Component {
 
@@ -23,8 +24,8 @@ class App extends Component {
     }
 
     render() {
-        if (!this.props.initialized){
-            return <Preloader />
+        if (!this.props.initialized) {
+            return <Preloader/>
         }
         return (
             <div className='app-wrapper'>
@@ -34,7 +35,12 @@ class App extends Component {
                     <Route path='/profile/:userId?'
                            render={() => <ProfileContainer/>}/>
                     <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}/>
+                           render={() => {
+                               return <Suspense fallback={<Preloader/>}>
+                                   <DialogsContainer/>
+                               </Suspense>
+                           }
+                           }/>
                     <Route path='/users'
                            render={() => <UsersContainer/>}/>
                     <Route path='/news' component={News}/>
@@ -54,7 +60,7 @@ const mapStateToProps = (state) => {
 }
 
 
-let ContainerApp =  connect(mapStateToProps, {initializeApp})(App)
+let ContainerApp = connect(mapStateToProps, {initializeApp})(App)
 
 let MainApp = () => {
     return (
